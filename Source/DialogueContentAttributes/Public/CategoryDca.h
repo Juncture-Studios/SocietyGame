829677176cecb31b16c35a02f0f43f156b68a434
Kai Chang <61954674+duova@users.noreply.github.com>
2024-07-17 16:57:13 +0100

    CategoryDca format.

---
+++ Source/DialogueContentAttributes/Public/CategoryDca.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DialogueContentAttribute.h"
+#include "GameplayTagContainer.h"
+#include "CategoryDca.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIALOGUECONTENTATTRIBUTES_API UCategoryDca : public UDialogueContentAttribute
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FGameplayTag Category;
+
+	virtual bool ContainsPhrase(const FString Phrase) const override;
+};
